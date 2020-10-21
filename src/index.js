c6c54ed28291b23a5034d8f751335723115d1883
rin-name <marinka_kostyuk@inbox.ru>
2020-10-22 01:09:52 +0300

    feat: the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+        n = n.toString();
+        a = n.length;
+    if (a === 3) {
+        return n.split("").reverse().join("");
+            } else  {
+         let b = n.slice(1,4);
+
+             return b.split("").reverse().join("");
+    }
+        }
+
+
